690ab197e2c09180a09beb9620bdb85ccc4fcaff
Awekin <semeniyk007@gmail.com>
2026-05-24 23:06:56 +0200

    111

--- src/main/java/org/example/Validator.java
+++ src/main/java/org/example/Validator.java
@@ -1,18 +1,23 @@
 package org.example;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 class Validator {
-    public static void valid(String filePath) throws FileNotFoundException {
+    public static void valid(String fileName) {
         String regex = "[0-9]{3}-[0-9]{3}-[0-9]{4}|\\([0-9]{3}\\) [0-9]{3}-[0-9]{4}";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
+        InputStream inputStream = Validator.class.getClassLoader().getResourceAsStream(fileName);
+        if (inputStream == null) {
+            System.err.println("Помилка: Файл " + fileName + " не знайдено у resources.");
+            return;
+        }
 
-            while ((line = br.readLine()) != null) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
                 if (line.matches(regex)) {
                     System.out.println(line);
                 }
